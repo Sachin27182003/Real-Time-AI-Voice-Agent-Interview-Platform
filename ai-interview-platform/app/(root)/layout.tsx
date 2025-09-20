8bb7d7e7090c80ec93d736e0a776708b8075d01f
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2025-09-20 13:38:13 +0530

    Added Root directory

---
+++ ai-interview-platform/app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const RootLayout = ({children} : {children : ReactNode}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RootLayout;
