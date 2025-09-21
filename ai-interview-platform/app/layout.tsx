ac3c10d6c21e53f2eca80a5bd4daa16fcbeb0c67
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2025-09-21 15:10:35 +0530

    working with AI

--- ai-interview-platform/app/layout.tsx
+++ ai-interview-platform/app/layout.tsx
@@ -1,32 +1,30 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "sonner";
 
-const MonaSans = Mona_Sans({
+import "./globals.css";
+
+const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "PrepWise",
-  description: "An AI Powered platform for preparing for mock interview",
+  description: "An AI-powered platform for preparing for mock interviews",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
-      <body
-        suppressHydrationWarning
-        className={`${MonaSans.className} antialiased pattern`}
-      >
+    <html lang="en" className="dark">
+      <body suppressHydrationWarning className={`${monaSans.className} antialiased pattern` }>
         {children}
-        <Toaster/>  
+
+        <Toaster />
       </body>
     </html>
   );
